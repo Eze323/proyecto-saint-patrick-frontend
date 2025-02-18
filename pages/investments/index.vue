acfade93feeccf6ad7e6b5a0a4ffa0c9d91627dd
Eze <a.ezequielirace@gmail.com>
2025-02-18 20:59:36 -0300

    se crea la estructura de paginas basicas

---
+++ pages/investments/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Inversiones</h1>
+        <p>Bienvenido a la página de inversiones.</p>
+        <!-- Agrega más contenido aquí -->
+    </div>
+</template>
+
+<script setup lang="ts">
+  definePageMeta({ layout: "authenticated" });
+  </script>
+
---
+++ pages/transfers/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="transfers-page">
+        <h1>Transferencias</h1>
+        <p>Aquí puedes realizar transferencias entre tus cuentas.</p>
+    
+        <div class="transfers-grid">
+        <TransferForm />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+  definePageMeta({ layout: "authenticated" });
+</script>
