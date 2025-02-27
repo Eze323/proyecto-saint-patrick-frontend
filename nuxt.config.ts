04f1222015abbc7b94327011145b96413a7ee4e9
Eze <a.ezequielirace@gmail.com>
2025-02-27 13:36:27 -0300

    actualizacion de favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head: {
+      title: 'Banco Saint Patrick',
+      meta: [
+        { name: 'description', content: 'App HomeBank' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
